a0eb297d6c367454dc1231a86d05379d9aad8be5
Ryan Stewart <stewart.rya@husky.neu.edu>
2016-11-22 16:02:45 -0500

    Made quick physics based player controller.  Added controls to player in test room

---
+++ Assets/SampleAssets/Scripts/SamplePlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamplePlayerMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float jump_speed = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            jump_speed = 0.5f;
+        }
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, rb.velocity.y+jump_speed);
+	}
+}
