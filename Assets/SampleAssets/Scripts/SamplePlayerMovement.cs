d8e2a444d3d95090ceb84b856630332f1db83e23
Ryan Stewart <stewart.rya@husky.neu.edu>
2016-12-02 15:53:50 -0500

    Fixed up teleport through door, added placeholder for walk between doors

--- Assets/SampleAssets/Scripts/SamplePlayerMovement.cs
+++ Assets/SampleAssets/Scripts/SamplePlayerMovement.cs
@@ -5,6 +5,7 @@ public class SamplePlayerMovement : MonoBehaviour {
 
     public float speed = 5.0f;
     private Rigidbody2D rb;
+    private bool hasControl = true;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +14,45 @@ public class SamplePlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float jump_speed = 0f;
-        if (Input.GetAxis("Vertical") > 0)
+        if (hasControl)
         {
-            jump_speed = 0.5f;
+            float jump_speed = 0f;
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                jump_speed = 0.5f;
+            }
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y + jump_speed);
         }
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, rb.velocity.y+jump_speed);
 	}
+
+    // Teleport between doors
+    public void TeleportBetweenRooms(Door door)
+    {
+        RoomManager rm = FindObjectOfType<RoomManager>();
+        rm.SetCurrentRoom(door.GetDestinationDoor().GetMyRoom());
+        this.gameObject.transform.position = door.GetDestinationDoor().GetMyDestination().position;
+        rm.CleanUpRooms();
+    }
+
+    // Walk between doors
+    public void WalkBetweenRooms(Door door)
+    {
+        // Remove player control
+        hasControl = false;
+        RoomManager rm = FindObjectOfType<RoomManager>();
+        rm.SetCurrentRoom(door.GetDestinationDoor().GetMyRoom());
+        this.gameObject.transform.position = door.GetDestinationDoor().GetMyDestination().position;
+        rm.CleanUpRooms();
+
+        // Reenable Control
+        hasControl = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Door>() != null)
+        {
+            TeleportBetweenRooms(other.GetComponent<Door>());
+        }
+    }
 }
