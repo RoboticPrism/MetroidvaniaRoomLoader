3745f89d8c49886081033ddf59f7fa815bf2e414
fenrisulfr <aussien8@hotmail.com>
2016-12-16 00:00:55 -0500

    Moved player movement to the rest of the scripts.  Deleted unneeded scripts

--- Assets/Scripts/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tags {
-
-	public static readonly string ROOM = "Room";
-	public static readonly string DOOR = "Door";
-	public static readonly string PLAYER = "Player";
-}
